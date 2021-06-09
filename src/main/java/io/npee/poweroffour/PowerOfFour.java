e0d585e3038a539ffe68079b815d32742b5c3797
npee <jmc4664@gmail.com>
2021-06-09 20:09:09 +0900

    power of four

---
+++ src/main/java/io/npee/poweroffour/PowerOfFour.java
@@ -0,0 +1,24 @@
+package io.npee.poweroffour;
+
+import java.util.Scanner;
+
+public class PowerOfFour {
+    public static boolean isPowerOfFour(int n) {
+        if ((n % 4 != 0 && n != 1) || (n <= 0)) return false;
+        if (n == 1) return true;
+        return isPowerOfFour(n / 4);
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Input > ");
+        int input = s.nextInt();
+        while (input > 0) {
+            System.out.println("Input: " + input);
+            System.out.println("Output: " + isPowerOfFour(input));
+            System.out.print("Input > ");
+            input = s.nextInt();
+        }
+
+    }
+}
