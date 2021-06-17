7db65ba0c96e9371333112d65b7d78f84837ee95
npee <jmc4664@gmail.com>
2021-06-17 23:55:28 +0900

    Gray Code 솔루션

---
+++ src/main/java/io/npee/graycode/Solution.java
@@ -0,0 +1,18 @@
+package io.npee.graycode;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public static List<Integer> grayCode(int n) {
+        Integer[] arr = new Integer[1 << n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i ^ (i >> 1);
+        }
+        return Arrays.asList(arr);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Output: " + grayCode(3));
+    }
+}
