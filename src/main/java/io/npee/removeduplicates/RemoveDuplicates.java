ab1c647b829f9d76ed4b5d1ecc8566e695003099
npee <jmc4664@gmail.com>
2021-06-28 22:36:32 +0900

    remove duplicates

---
+++ src/main/java/io/npee/removeduplicates/RemoveDuplicates.java
@@ -0,0 +1,25 @@
+package io.npee.removeduplicates;
+
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+    public static String removeDuplicates(String s) {
+
+        if (s.length() == 1) return s;
+        int ptr = 0;
+        char[] chars = s.toCharArray();
+        for (int idx = 0; idx < chars.length; ++ptr, ++idx) {
+            chars[ptr] = chars[idx];
+            if (ptr > 0 && chars[ptr - 1] == chars[ptr]) {
+                ptr -= 2;
+            }
+        }
+        return new String(chars, 0, ptr);
+    }
+
+    public static void main(String[] args) {
+        // String s = "ibfjcaffccadidiaidchakchchcahabhibdcejkdkfbaeeaecdjhajbkfebebfea";
+        String s = "awzzxxwy";
+        System.out.println(removeDuplicates(s));
+    }
+}
