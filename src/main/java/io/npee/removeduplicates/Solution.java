d3eb161bf5b4bfd87ed16225a676d7b0aeb97855
npee <jmc4664@gmail.com>
2021-06-30 21:21:31 +0900

    remove duplicates using StringBuilder

---
+++ src/main/java/io/npee/removeduplicates/Solution.java
@@ -0,0 +1,22 @@
+package io.npee.removeduplicates;
+
+public class Solution {
+    public static String removeDuplicates(String S) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : S.toCharArray()) {
+            int size = sb.length();
+            if (size > 0 && sb.charAt(size - 1) == c) {
+                sb.deleteCharAt(size - 1);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        // String s = "ibfjcaffccadidiaidchakchchcahabhibdcejkdkfbaeeaecdjhajbkfebebfea";
+        String s = "awzzxxwy";
+        System.out.println(removeDuplicates(s));
+    }
+}
